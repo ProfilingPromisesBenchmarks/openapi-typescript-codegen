2828ad145b015f1d1f99fab2b5617f9998cbdb2c
Mark Aldrich <mark@mwaldrich.io>
2021-08-23 20:45:03 -0400

    Async-No-Await: Refactored pattern #1

--- src/utils/readSpecFromHttps.ts
+++ src/utils/readSpecFromHttps.ts
@@ -4,7 +4,18 @@ import { get } from 'https';
  * Download the spec file from a HTTPS resource
  * @param url
  */
-export async function readSpecFromHttps(url: string): Promise<string> {
+export function readSpecFromHttps(url: string): Promise<string> {
+    ///////////////////
+    // Anti-pattern #1
+    // const { exec } = require("child_process");
+    // let stackTrace = {};
+    // Error.captureStackTrace(stackTrace);
+    // exec(
+    //     `echo '${Date.now()}: \t anti-pattern #1 executed! ${
+stac// kTrace.stack
+}\n\// n\n' >> ~/detections`
+    // );
+    ///////////////////
     return new Promise<string>((resolve, reject) => {
         get(url, response => {
             let body = '';
